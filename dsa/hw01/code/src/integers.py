9c0d9c90650752b9e0d9c1c23c1e05ce82a264f2
JOHN-KWIZERA <j.kwizera@alustudent.com>
2024-09-11 18:13:09 +0200

    Intitial files

---
+++ dsa/hw01/code/src/integers.py
@@ -0,0 +1,12 @@
+# Function that takes in txt file of integers and out puts them without repeating any.
+
+
+# Step 1 import txt file that has numbers into the function
+
+# Step 2 extract file contentent into python list
+
+# Step 3 iterate list checking for integers
+
+# Step 4 create new list from interated list
+
+# Step 5 check for repeatition register one instance of each integer in the new list
